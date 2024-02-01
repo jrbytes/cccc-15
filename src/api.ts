2512e0bd4fe2c3d8129faba0a80efd9f0d337820
Junior Bytes <itamazor@gmail.com>
2024-02-01 15:28:27 -0300

    adding ride

--- src/api.ts
+++ src/api.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import { AccountDAODatabase } from './AccountDAO';
 import Signup from './Signup';
 import GetAccount from './GetAccount';
+import { v4 } from 'uuid';
+import pgp from 'pg-promise';
 const app = express();
 app.use(express.json());
 app.post('/signup', async (req, res) => {
@@ -15,5 +17,27 @@ app.get('/accounts/:accountId', async (req, res) => {
   const getAccount = new GetAccount(accountDAO);
   const output = await getAccount.execute(req.params.accountId)
   res.json(output)
+})
+app.post('/request_ride', async (req, res) => {
+  const rideId = v4()
+  const connection = pgp()("postgres://postgres:123456@localhost:5432/cccat15")
+  const [account] = await connection.query("SELECT * FROM cccat15.account where account_id = $1", [req.body.passengerId])
+  if (!account.is_passenger) {
+    return res.status(422).json({ message: 'Account is not from a passenger' })
+  }
+  const [activeRide] = await connection.query("SELECT * FROM cccat15.ride where passenger_id = $1 AND status = 'requested'", [req.body.passengerId])
+  if (activeRide) {
+    return res.status(422).json({ message: 'Passenger has an active ride' })
+  }
+  await connection.query("INSERT INTO cccat15.ride (ride_id, passenger_id, from_lat, from_long, to_lat, to_long, status, date) values ($1, $2, $3, $4, $5, $6, $7, $8)",
+  [rideId, req.body.passengerId, req.body.fromLat, req.body.fromLong, req.body.toLat, req.body.toLong, 'requested', new Date()])
+  await connection.$pool.end()
+  res.json({ rideId })
+})
+app.get('/rides/:rideId', async (req, res) => {
+  const connection = pgp()("postgres://postgres:123456@localhost:5432/cccat15")
+  const [ride] = await connection.query("SELECT * FROM cccat15.ride where ride_id = $1", [req.params.rideId])
+  await connection.$pool.end()
+  res.json(ride)
 })
 app.listen(3000);
--- test/api.test.ts
+++ test/api.test.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+axios.defaults.validateStatus = () => true
+
 it('deve cadastrar uma conta de passageiro', async () => {
   const input = {
     name: "Junior Bytes",
@@ -25,4 +27,81 @@ it('deve cadastrar uma conta de passageiro', async () => {
     cpf: input.cpf,
     is_passenger: input.isPassenger,
   })
+})
+
+it('deve solicitar uma corrida', async () => {
+  const inputSignup = {
+    name: "Junior Bytes",
+    email: `johndoe${Math.random()}@gmail.com`,
+    cpf: '123.456.789-09',
+    isPassenger: true,
+    isDriver: false
+  }
+  const responseSignup = await axios.post('http://localhost:3000/signup', inputSignup);
+  const outputSignup = responseSignup.data;
+  const inputRequestRide = {
+    passengerId: outputSignup.accountId,
+    fromLat: -27.5630991,
+    fromLong: -48.6565712,
+    toLat: -27.581092,
+    toLong: -48.593673,
+  }
+  const responseRequestRide = await axios.post('http://localhost:3000/request_ride', inputRequestRide)
+  const outputRequestRide = responseRequestRide.data;
+  expect(outputRequestRide.rideId).toBeDefined();
+  const responseGetRide = await axios.get(`http://localhost:3000/rides/${outputRequestRide.rideId}`)
+  const outputGetRide = responseGetRide.data
+  expect(responseRequestRide.status).toBe(200)
+  expect(outputGetRide.passenger_id).toBe(inputRequestRide.passengerId)
+  expect(outputGetRide.ride_id).toBe(outputRequestRide.rideId)
+  expect(outputGetRide.from_lat).toBe(String(inputRequestRide.fromLat))
+  expect(outputGetRide.status).toBe('requested')
+  expect(outputGetRide.date).toBeDefined()
+})
+
+it('não deve solicitar uma corrida se não for passageiro', async () => {
+  const inputSignup = {
+    name: "Junior Bytes",
+    email: `johndoe${Math.random()}@gmail.com`,
+    cpf: '123.456.789-09',
+    carPlate: 'ABC1234',
+    isPassenger: false,
+    isDriver: true,
+  }
+  const responseSignup = await axios.post('http://localhost:3000/signup', inputSignup);
+  const outputSignup = responseSignup.data;
+  const inputRequestRide = {
+    passengerId: outputSignup.accountId,
+    fromLat: -27.5630991,
+    fromLong: -48.6565712,
+    toLat: -27.581092,
+    toLong: -48.593673,
+  }
+  const responseRequestRide = await axios.post('http://localhost:3000/request_ride', inputRequestRide)
+  const outputRequestRide = responseRequestRide.data;
+  expect(responseRequestRide.status).toBe(422)
+  expect(outputRequestRide.message).toBe('Account is not from a passenger')
+})
+
+it('não deve solicitar uma corrida se o passageiro tiver outra corrida com outra corrida ativa', async () => {
+  const inputSignup = {
+    name: "Junior Bytes",
+    email: `johndoe${Math.random()}@gmail.com`,
+    cpf: '123.456.789-09',
+    isPassenger: true,
+  }
+  const responseSignup = await axios.post('http://localhost:3000/signup', inputSignup);
+  const outputSignup = responseSignup.data;
+  const inputRequestRide = {
+    passengerId: outputSignup.accountId,
+    fromLat: -27.5630991,
+    fromLong: -48.6565712,
+    toLat: -27.581092,
+    toLong: -48.593673,
+  }
+  await axios.post('http://localhost:3000/request_ride', inputRequestRide)
+  const responseRequestRide = await axios.post('http://localhost:3000/request_ride', inputRequestRide)
+  const outputRequestRide = responseRequestRide.data;
+  expect(responseRequestRide.status).toBe(422)
+  expect(outputRequestRide.message).toBe('Passenger has an active ride')
 })
