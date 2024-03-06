49bceaf892c2cf6b147bbc8b6cc3e02b98553725
Junior Bytes <itamazor@gmail.com>
2024-03-06 08:17:46 -0300

    update ORM test to find by email

--- test/integration/ORM.test.ts
+++ test/integration/ORM.test.ts
@@ -33,7 +33,7 @@ it('deve testar o ORM', async () => {
   await connection.close()
 })
 
-it('deve testar o ORM com um aggregate real', async () => {
+it('deve testar o ORM com um aggregate real, findBy account_id', async () => {
   const account = Account.create(
     'John Doe',
     'john@gmail.com',
@@ -50,6 +50,33 @@ it('deve testar o ORM com um aggregate real', async () => {
     AccountModel,
     'account_id',
     account.accountId,
+  )
+  expect(getAccountModel.name).toBe(accountModel.name)
+  expect(getAccountModel.email).toBe(accountModel.email)
+  expect(getAccountModel.cpf).toBe(accountModel.cpf)
+  expect(getAccountModel.carPlate).toBe(accountModel.carPlate)
+  expect(getAccountModel.isPassenger).toBe(accountModel.isPassenger)
+  expect(getAccountModel.isDriver).toBe(accountModel.isDriver)
+  await connection.close()
+})
+
+it('deve testar o ORM com um aggregate real, findBy email', async () => {
+  const account = Account.create(
+    'John Doe',
+    'john@gmail.com',
+    '97456321558',
+    true,
+    false,
+    '',
+  )
+  const accountModel = AccountModel.fromAggregate(account)
+  const connection = new PgPromiseAdapter()
+  const orm = new ORM(connection)
+  await orm.save(accountModel)
+  const getAccountModel = await orm.findBy(
+    AccountModel,
+    'email',
+    account.getEmail(),
   )
   expect(getAccountModel.name).toBe(accountModel.name)
   expect(getAccountModel.email).toBe(accountModel.email)
